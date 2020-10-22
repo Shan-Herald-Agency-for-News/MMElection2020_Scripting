48cc4fb6044b76a1c84eff0c31529bd10714cfd3
NoerNova <noernova666@gmail.com>
2020-10-22 13:47:26 +0700

    add shan state region candidate image download

--- imgDownload.py
+++ imgDownload.py
@@ -3,31 +3,67 @@ import requests
 import os
 import shutil
 
-image_url = ""
-filename = ""
-filepath = ""
+
+def partyCandidates_imgDownloads():
+    image_url = ""
+    filename = ""
+    filepath = ""
+
+    with open('shanRegionCandidates.json') as jFile:
+        data = json.load(jFile)
+
+        for d in data['data']:
+            attr = d['attributes']
+            image_url = attr['image']
+            filename = filename.join([attr['name'], "_", d['id'], ".jpg"])
+            filepath = os.path.join("candidatesImg", filename)
+
+            r = requests.get(image_url, stream=True)
+
+            if r.status_code == 200:
+                r.raw.decode_content = True
+
+                with open(filepath, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+
+                print("Image successfully Downloaded: ", filename)
+                image_url = ""
+                filename = ""
+                filepath = ""
+            else:
+                print("Image Couldn\'t be retreived")
 
 
-with open('shanRegionCandidates.json') as jFile:
-    data = json.load(jFile)
+def shanStateCandidates_imgDownloads():
+    image_url = ""
+    filename = ""
+    filepath = ""
 
-    for d in data['data']:
-        attr = d['attributes']
-        image_url = attr['image']
-        filename = filename.join([attr['name'], "_", d['id'], ".jpg"])
-        filepath = os.path.join("candidatesImg", filename)
+    with open('shan_state_regionCandidates.json') as jFile:
+        data = json.load(jFile)
 
-        r = requests.get(image_url, stream=True)
+        for d in data['data']:
+            attr = d['attributes']
+            image_url = attr['image']
+            filename = filename.join([attr['name'], "_", d['id'], ".jpg"])
+            filepath = os.path.join(
+                "candidatesImg/shanStateRegionCandidates", filename)
 
-        if r.status_code == 200:
-            r.raw.decode_content = True
+            r = requests.get(image_url, stream=True)
 
-            with open(filepath, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
+            if r.status_code == 200:
+                r.raw.decode_content = True
 
-            print("Image successfully Downloaded: ", filename)
+                with open(filepath, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+
+                print("Image successfully Downloaded: ", filename)
+            else:
+                print("Image Couldn\'t be retreived", filename)
+
             image_url = ""
             filename = ""
             filepath = ""
-        else:
-            print("Image Couldn\'t be retreived")
+
+
+shanStateCandidates_imgDownloads()
